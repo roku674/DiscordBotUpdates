42e3b05750231d04b4cc0a470743373840fdd29d
roku674 <af03848@georgiasouthern.edu>
2022-06-06 20:07:48 -0400

    commit

---
+++ Modules/Commands.cs
@@ -0,0 +1,51 @@
+﻿//Created by Alexander Fields https://github.com/roku674
+using Discord.Commands;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotUpdates.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+
+        [Command("Destroy NWO")]
+        public async Task DestroyNWO()
+        {
+            await ReplyAsync("By Your Command!");
+        }
+
+        [Command("run BotUpdater")]
+        public async Task BotUpdater()
+        {
+            await ReplyAsync("By Your Command!");
+
+            for (int i = 0; i < 86400; i++)
+            {
+                await Task.Delay(1000);
+
+                if (File.Exists(Directory.GetCurrentDirectory() + "/botUpdates.txt"))
+                {
+                    Program.botUpdates = File.ReadAllText(Directory.GetCurrentDirectory() + "/botUpdates.txt");
+
+                    if (!string.IsNullOrEmpty(Program.botUpdates))
+                    {
+                        System.Console.WriteLine(Program.botUpdates);
+                        await ReplyAsync(Program.botUpdates);
+                        File.WriteAllText(Directory.GetCurrentDirectory() + "/botUpdates.txt", "");
+                    }
+                }
+                else
+                {
+                    File.Create(Directory.GetCurrentDirectory() + "/botUpdates.txt");
+                }
+            }
+            await ReplyAsync("No Longer Checking Bot Updates!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//Created by Alexander Fields https://github.com/roku674
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BotUpdates
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private static string _botUpdates = "";
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public static string botUpdates { get => _botUpdates; set => _botUpdates = value; }
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "OTgzNDY2ODI0ODE5Njc1MTQ2.GBMlyr.WlqZ7ccfgpPHIJm7iTfo2GbggmLR8h2nOKhb-4";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage message = arg as SocketUserMessage;
+            SocketCommandContext context = new SocketCommandContext(_client, message);
+
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("$", ref argPos))
+            {
+                IResult result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
