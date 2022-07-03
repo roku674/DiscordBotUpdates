91ae390cc453d976ef348bd4acc8e3e4d30cf123
roku674 <af03848@georgiasouthern.edu>
2022-07-03 11:27:26 -0400

    batch for git pulling the other repos

---
+++ git.bat
@@ -0,0 +1,8 @@
+git.exe fetch
+git.exe pull
+cd H:\My Drive\Shared\DiscordBotUpdates\\DiscordBotUpdates\Objects\StarportObjects
+git.exe fetch
+git.exe pull
+cd H:\My Drive\Shared\DiscordBotUpdates\\DiscordBotUpdates\\Algorithms-for-C-Sharp
+git.exe fetch
+git.exe pull
